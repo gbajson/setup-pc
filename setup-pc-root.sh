942edfdcd761cca71059c90f63becac0f7ce6d95
Grzegorz Bajson <gbajson@protonmail.ch>
2025-10-11 15:15:18 +0000

    Install gh cli

--- setup-pc-root.sh
+++ setup-pc-root.sh
@@ -101,6 +101,25 @@ if ! command -v go &> /dev/null; then
   fi
 fi
 
+
+if ! command -v gh &> /dev/null; then
+  if [[ $(lsb_release -i | grep -i "ubuntu") ]]; then
+    (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+  else
+    echo "Please install gh cli."
+    exit 1
+  fi
+fi
+
+
 ansible localhost -c local -b -m ansible.builtin.group -a "name=$user gid=$gid state=present"
 ansible localhost -c local -b -m ansible.builtin.user -a "name=$user uid=$uid group=$user groups=docker,sudo append=yes create_home=yes shell=/bin/bash state=present"
 
